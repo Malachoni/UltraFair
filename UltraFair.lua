aae25a77de0351657fc6b44ade4029f33ac15611
MrLevi <107175487+Malachoni@users.noreply.github.com>
2023-12-17 09:48:49 +0200

    Update UltraFair.lua

--- UltraFair.lua
+++ UltraFair.lua
@@ -64,7 +64,11 @@ AuraSettings = Load("AuraSettings")
 RollSettings = Load("RollSettings")
 FarmSettings = Load("FarmSettings")
 
-
+local VirtualUser=game:service'VirtualUser'
+game:service'Players'.LocalPlayer.Idled:connect(function()
+VirtualUser:CaptureController()
+VirtualUser:ClickButton2(Vector2.new())
+end)
 
 local Old = getsenv(Player.PlayerScripts.MoveHandler)
 
